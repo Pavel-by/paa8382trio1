68008c2bd65478206a790301ba24bcfc9c1dbee2
Pavel Mironchik <mairon-pasha@yandex.ru>
2020-06-24 18:28:16 +0300

    Initial commit

---
+++ src/main/kotlin/ui/TrioApp.kt
@@ -0,0 +1,9 @@
+package ui
+
+import javafx.scene.Scene
+import javafx.stage.Stage
+import tornadofx.App
+import tornadofx.Workspace
+import tornadofx.titledpane
+
+class TrioApp : App(Workspace::class)
