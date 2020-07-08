7eafeaae329fcb918576cfa55267a9d0eeae739b
max@max <sinelnikovmaxim2000@mail.ru>
2020-07-08 14:35:37 +0300

    something added

---
+++ src/test/kotlin/model/BoruvkaAlgorithmTest.kt
@@ -0,0 +1,16 @@
+package model
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class BoruvkaAlgorithmTest {
+
+    @Test
+    fun getGraphIsConnected() {
+    }
+
+    @Test
+    fun process() {
+    }
+}
--- src/test/kotlin/model/GraphGeneratorTest.kt
+++ src/test/kotlin/model/GraphGeneratorTest.kt
@@ -1,9 +1,13 @@
 package model
 
+import javafx.scene.control.TreeItem
 import javafx.scene.control.TreeView
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
+import tornadofx.onChange
+import ui.controller.TreeViewController
+import ui.TrioWorkspace
 
 class GraphGeneratorTest {
 
@@ -21,8 +25,24 @@ class GraphGeneratorTest {
 
     @Test
     fun process() {
-        val treeview = TreeView
-        val graph = GraphGenerator()
-        graph.process()
+        val treeView = ui.views.TreeView().apply {
+            controller = TreeViewController(this).apply {
+                addEdgeSelectedProperty.onChange {}
+                addNodeSelectedProperty.onChange { updateButtons() }
+                currentStepProperty.onChange { updateButtons() }
+                maxStepProperty.onChange { updateButtons() }
+                editProperty.onChange { TrioWorkspace. }
+            }
+        }
+//        val graph = GraphGenerator()
+//        val number_of_iterations = 1000
+//        var count = 0;
+//        while (count < number_of_iterations){
+//            val numer_of_nodes = (0..100).random()
+//            val number_of_edges = (0..100).random()
+//            graph.process(treeview,numer_of_nodes,number_of_edges)
+//            assertEquals(treeview.treeModel.nodes.size == numer_of_nodes && treeview.treeModel.edges.size == number_of_edges,true)
+//            count++
+//        }
     }
 }
